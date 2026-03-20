bc2c7ff756bd06a15865590a30c4e7c6ee137662
Ragh2007 <raghavsinhavns@gmail.com>
2026-03-21 00:30:16 +0530

    Refactor PORT assignment and update API route

--- agriculture/backend/index.js
+++ agriculture/backend/index.js
@@ -6,16 +6,20 @@ import apiRoutes from './routes/api.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
+
 
 app.use('/api', apiRoutes);
 
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
-});
-app.get('/', (req, res) => {
-  res.send('API is running 🚀');
 });
